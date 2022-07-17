6266cad2f37112ce33b22364a175a5c79afa28fd
AnishG17 <anishglinked@gmail.com>
2022-07-17 14:46:00 +0530

    New mail sender

--- email_sender/esender/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from esender import views
-urlpatterns = [
-    path('',views.index,name='index')
-    # path('',include('esender.urls')),
-    # path('admin/', admin.site.urls),
-]

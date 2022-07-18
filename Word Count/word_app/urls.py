434cb2ecf77b3905ad19cd95a2dce91d0417a136
AnishG17 <anishglinked@gmail.com>
2022-07-18 10:38:06 +0530

    Word Counter

--- Word Count/word_app/urls.py
+++ Word Count/word_app/urls.py
@@ -1,10 +1,8 @@
-from django.contrib import admin
 from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-  
+from word_app import views
+# from django.conf import settings
+# from django.conf.urls.static import static
+
 urlpatterns = [
-    path('',views.counter1, name='counter1'),
-    # path('admin/', admin.site.urls),
+	path('counter',views.counter, name='counter'),
 ]

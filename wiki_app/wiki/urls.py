181cda31227772b84d93ba40b76c7f302c38c89b
AnishG17 <anishglinked@gmail.com>
2022-07-29 10:08:29 +0530

    New

--- wiki_app/wiki/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from wiki import views
-urlpatterns = [
-    path('',views.index,name='index')
-    # path('',include('wiki.urls')),
-    # path('admin/', admin.site.urls),
-]

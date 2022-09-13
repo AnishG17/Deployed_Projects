84e3987e3b02ceff3537de9f6c13d10a49d7f47a
Anish Gandre <109129179+AnishG17@users.noreply.github.com>
2022-09-13 23:07:57 +0530

    Delete Medront/Eccom directory

--- Medront/Eccom/README.md
+++
@@ -1,1 +0,0 @@
-Please check the models.py and views.py file, all the main codes are there. No html pages are made.
--- Medront/Eccom/home/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns = [
-    path('',views.index,name='index'),
-    path('',views.customer_signup,name='customer_signup'),
-    path('',views.customer_login,name='customer_login'),
-]
